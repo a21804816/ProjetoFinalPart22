86d7c31e9aaadf31e4b8087ff753f22fde641381
AfonsoMmsc <afonsonogueirammsc@gmail.com>
2023-04-24 18:52:00 +0100

    muitas mudanças

--- app/src/main/java/com/example/projetofinalpart1/fragments/DetalhesFragment.kt
+++ app/src/main/java/com/example/projetofinalpart1/fragments/DetalhesFragment.kt
@@ -85,6 +85,7 @@ class DetalhesFragment : Fragment() {
             binding.observacoes.isEnabled = true
 
 
+
             binding.editButton.setBackgroundResource(R.drawable.baseline_check_circle_24)
 
             binding.editButton.setOnClickListener {
