7b77b1d43b0dbc546b1f24b0cf5f9b22c7ceb33e
AfonsoMmsc <afonsonogueirammsc@gmail.com>
2023-04-24 18:59:00 +0100

    muitas mudanças

--- app/src/main/java/com/example/projetofinalpart1/fragments/DetalhesFragment.kt
+++ app/src/main/java/com/example/projetofinalpart1/fragments/DetalhesFragment.kt
@@ -86,7 +86,6 @@ class DetalhesFragment : Fragment() {
 
             binding.editButton.setBackgroundResource(R.drawable.baseline_check_circle_24)
 
-
             binding.editButton.setOnClickListener {
                 val filme = ObjetoFilme.getOperationById(filmeUuid!!)
                 if (filme != null) {
