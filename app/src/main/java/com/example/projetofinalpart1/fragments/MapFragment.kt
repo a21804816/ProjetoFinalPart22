78a6ccd8d1d6946d25bdca2d1ab64c6f64f683be
rcunha8 <a21804816@alunos.ulht.pt>
2023-05-31 00:47:44 +0100

    mapa

--- app/src/main/java/com/example/projetofinalpart1/fragments/MapFragment.kt
+++ app/src/main/java/com/example/projetofinalpart1/fragments/MapFragment.kt
@@ -1,29 +1,53 @@
 package com.example.projetofinalpart1.fragments
 
-
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.projetofinalpart1.R
 import com.example.projetofinalpart1.databinding.FragmentMapBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.LocationSource
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.LocationSource.OnLocationChangedListener
 
-class MapFragment : Fragment() {
+
+
+class MapFragment : Fragment(){
 
     private lateinit var binding: FragmentMapBinding
+    private var map: GoogleMap? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        val view = inflater.inflate(
-            R.layout.fragment_map, container, false
-        )
+        val view = inflater.inflate(R.layout.fragment_map, container, false)
         binding = FragmentMapBinding.bind(view)
+        binding.map.onCreate(savedInstanceState)
+        binding.map.getMapAsync { googleMap ->
+            map = googleMap
 
-
+                map?.isMyLocationEnabled = true
+                map?.addMarker(
+                    MarkerOptions()
+                        .position(LatLng(38.75814, -9.15179))
+                        .title("ULHT")
+                )
+        }
         return binding.root
     }
 
-
+    override fun onResume() {
+        super.onResume()
+        binding.map.onResume()
+    }
 }
