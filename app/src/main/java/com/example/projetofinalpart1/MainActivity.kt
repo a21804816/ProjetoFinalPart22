169e2a8d22a39249595f3d168daec327f609954b
rcunha8 <a21804816@alunos.ulht.pt>
2023-06-12 14:16:19 +0100

    pesquisa por voz

--- app/src/main/java/com/example/projetofinalpart1/MainActivity.kt
+++ app/src/main/java/com/example/projetofinalpart1/MainActivity.kt
@@ -10,6 +10,7 @@ import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
+import com.example.projetofinalpart1.data.FilmDao
 import com.example.projetofinalpart1.data.FilmeRepository
 import com.example.projetofinalpart1.data.FilmeRoom
 import com.example.projetofinalpart1.data.FilmsDatabase
@@ -73,8 +74,6 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             }
-
-
             dialogBinding.cancelButton.setOnClickListener {
                 dialog.dismiss()
             }
